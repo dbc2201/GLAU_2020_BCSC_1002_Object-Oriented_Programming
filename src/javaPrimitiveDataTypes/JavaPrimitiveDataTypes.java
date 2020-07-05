2bb9751c96409abbb5bed0429eb4998f9795af80
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2020-07-05 13:57:31 +0530

    add comments to list primitive data types in Java

--- src/javaPrimitiveDataTypes/JavaPrimitiveDataTypes.java
+++ src/javaPrimitiveDataTypes/JavaPrimitiveDataTypes.java
@@ -8,6 +8,16 @@ package javaPrimitiveDataTypes;
 
 public class JavaPrimitiveDataTypes {
     public static void main(String[] args) {
-
+        /*
+         * There are 8 Primitive Data Types in Java
+         * 1. byte
+         * 2. char
+         * 3. short
+         * 4. int
+         * 5. long
+         * 6. float
+         * 7. double
+         * 8. boolean
+         * */
     }
 }
