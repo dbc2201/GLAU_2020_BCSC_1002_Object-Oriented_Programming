1fda06669ec3332dfd9f51535b0f2a9b3924d172
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2020-07-05 14:09:27 +0530

    add code for printing the ranges of data types

--- src/javaPrimitiveDataTypes/JavaPrimitiveDataTypes.java
+++ src/javaPrimitiveDataTypes/JavaPrimitiveDataTypes.java
@@ -26,5 +26,30 @@ public class JavaPrimitiveDataTypes {
         // We can access the number by using
         // Minimum Value: <data type class name>.MIN_VALUE
         // Maximum Value: <data type class name>.MAX_VALUE
+
+        // range for byte
+        System.out.println("Range of byte is from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+
+        // range for char
+        System.out.println("Range of char is from '" + Character.MIN_VALUE + "' to '" + Character.MAX_VALUE + "'");
+
+        // range for short
+        System.out.println("Range for short is from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+
+        // range for int
+        System.out.println("Range of int is from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+
+        // range for long
+        System.out.println("Range of long is from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+
+        // range for float
+        System.out.println("Range of float is from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+        // range for double
+        System.out.println("Range of double is from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+        // The boolean data type is not a numerical data type in Java as opposed to C.
+        // The Java Virtual Machine interprets the boolean true and false values as is and NOT as 0 or 1.
+        System.out.println("The boolean data type has the '" + Boolean.TRUE + "' and '" + Boolean.FALSE + "' values.");
     }
 }
