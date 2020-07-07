ed1fa8057b1ad2bf849a83aa5ed3a63b6bf6c5e3
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2020-07-07 09:23:56 +0530

    create class to demonstrate accepting input via the Scanner class

---
+++ src/acceptingEndUserInput/InputSteps.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *   Author: Divyansh Bhardwaj (dbc2201)
+ *   Date: 07/07/20
+ *   Time: 9:22 AM
+ */
+
+package acceptingEndUserInput;
+
+public class InputSteps {
+}
