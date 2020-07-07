374aa59524b3d264395f37c30c5d540e89e9051b
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2020-07-07 09:25:49 +0530

    create an object of the scanner class

--- src/acceptingEndUserInput/InputSteps.java
+++ src/acceptingEndUserInput/InputSteps.java
@@ -8,8 +8,13 @@ package acceptingEndUserInput;
 
 //  Step 1: import the Scanner class from the java.util package.
 
+import java.util.Scanner;
+
 public class InputSteps {
     public static void main(String[] args) {
-
+        // Step 2: create an object of the Scanner class
+        // use this formula to create an object of any class in Java
+        // <ClassName> <objectName> = new <ClassName>();
+        Scanner scanner = new Scanner(System.in);
     }
 }
